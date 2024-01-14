0faa932f29aa979f0d492c8e789af3604a3f807a
Vincenzo Tito <vincenzotito@icloud.com>
2024-01-14 09:36:59 +0100

    esercizi completati

--- php_02_Tito.php
+++ php_02_Tito.php
@@ -2,11 +2,11 @@
 
 // Traccia 1
 
-$users = [
-['name' =>'Mario', 'surname' => 'Rossi', 'gender' => 'M'],
-['name' =>'Giorgia', 'surname' => 'Bianchi', 'gender' => 'F'],
-['name' =>'Optimus', 'surname' => 'Prime', 'gender' => 'NB']
-];
+// $users = [
+// ['name' =>'Mario', 'surname' => 'Rossi', 'gender' => 'M'],
+// ['name' =>'Giorgia', 'surname' => 'Bianchi', 'gender' => 'F'],
+// ['name' =>'Optimus', 'surname' => 'Prime', 'gender' => 'NB']
+// ];
 
 // foreach($users as $elemento){
 //     if($elemento['gender']== "M"){
@@ -36,16 +36,16 @@ $users = [
 
       // Traccia 2
 
-      $numeri = [1,4,6,8,14,5,7];
-      $somma_totale = 0;
-      $counter = 0;
-    foreach($numeri as $numero) { 
+  //     $numeri = [1,4,6,8,14,5,7];
+  //     $somma_totale = 0;
+  //     $counter = 0;
+  //   foreach($numeri as $numero) { 
     
-        if(($numero % 2) == 0){
-        $somma_totale += $numero;
-        $counter++;
-       }
-   }
+  //       if(($numero % 2) == 0){
+  //       $somma_totale += $numero;
+  //       $counter++;
+  //      }
+  //  }
 
   // var_dump($somma_totale/$counter);
 
@@ -55,15 +55,51 @@ $users = [
 //   for($i = 0 ;$i<=100;$i++){
 
 //    if($i % 3 == 0 && $i % 5 == 0 ){
-//     echo "HACKADEMY"."\n";
+//     // echo "HACKADEMY"."\n";
 //    }else if ($i % 3 == 0){
-//     echo "PHP"."\n";
+//     // echo "PHP"."\n";
 //    }else if ($i % 5 == 0){
-//     echo "JAVASCRIPT"."\n";
+//     // echo "JAVASCRIPT"."\n";
 //    }else{
-//     echo $i."\n";
+//     // echo $i."\n";
 //    }
 // };
+
+
+// Traccia 4
+
+// $temperatura = 26;
+
+// if($temperatura <= 15){
+//   echo "Fa freddo";
+// }else if($temperatura > 25){
+//   echo "Fa molto caldo";
+// }else{
+//   echo "Fa caldo";
+// }
+
+
+// Traccia 5
+
+// $temperature = [
+//   "Venezia" => 16, "Bari" => 32, "Roma" => 28, "Napoli" => 30, "Milano" => 13, "Palermo" => 14, "Torino" => 2, "Lecce" => 27, "Genova" => 30, "Catania" => 11, "Cosenza" => 9
+// ];
+
+// foreach($temperature as $chiave => $valore){
+
+//   if($valore <= 15){
+//       echo "A" . ' ' .$chiave . ' ' .  "Fa freddo" . ' ' . "con" . ' ' . $valore . ' ' . "°C" .  "\n";
+//     }else if($valore > 25){
+//       echo "A" . ' ' .$chiave . ' ' .  "Fa molto caldo" . ' ' . "con" . ' ' . $valore . ' ' . "°C" ."\n";
+//     }else{
+//       echo "A" . ' ' .$chiave . ' ' .  "Fa caldo" . ' ' . "con" . ' ' . $valore . ' ' . "°C" . "\n";
+//     }
+// }
+
+
+
+
+
 
 
 
